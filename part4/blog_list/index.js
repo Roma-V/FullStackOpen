7a49f703d60380ad0111b5581bf97a3227cd57f6
RomanVasilyev <roman.m.vasilyev@gmail.com>
2020-11-22 18:36:54 +0300

    Ex.4.1 - A list of blog posts

---
+++ part4/blog_list/index.js
@@ -0,0 +1,91 @@
+/**
+ * @license
+ * Copyright (c) 2020 Example Corporation Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file contains main logic for the Blog List app.
+ * @author Roman Vasilyev 
+ */
+
+/*
+ * Imports
+ */
+const dotenv = require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+/*
+ * Implementation
+ */
+const app = express()
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+// const mongoUrl = 'mongodb://localhost:27017'
+mongoose.connect(mongoUrl, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false, 
+        useCreateIndex: true 
+    })
+    .then(result => {
+        console.log('Connected to DB', result)
+    }).catch(error => {
+        console.log('connection to DB failed', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
